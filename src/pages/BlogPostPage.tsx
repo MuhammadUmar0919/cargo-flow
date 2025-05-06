ae5af25bc31c709b5a9f489738513979ae834478
MuhammadYusuf-0919 <ixtiyor0919@gmail.com>
2025-05-06 21:45:00 +0500

    main image updated

--- src/pages/BlogPostPage.tsx
+++ src/pages/BlogPostPage.tsx
@@ -169,7 +169,7 @@ const BlogPostPage = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.1 }}
-              className="mb-10 vh-[20%] "
+              className="mb-10 vh-[15%] "
             >
               <img 
                 src={post.image} 
